1030cffcb1b75363644fab13b9cec55625f5c6f9
ManOfSteel1976 <57708230+ManOfSteel1976@users.noreply.github.com>
2022-03-31 21:47:57 +0200

    Tests 2 a 5

--- src/test/java/org/mps2022/practica3/AdvertisementBoardTest.java
+++ src/test/java/org/mps2022/practica3/AdvertisementBoardTest.java
@@ -2,44 +2,88 @@ package org.mps2022.practica3;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 class AdvertisementBoardTest {
 
+  @Mock
+  AdvertiserDatabase advDatabMock;
+
+  @Mock
+  PaymentDatabase paymDatabMock;
+
   public AdvertisementBoard board;
+
+  @BeforeEach
+  public void setup() {
+    board = new AdvertisementBoard();
+    MockitoAnnotations.initMocks(this);
+  }
 
   @Test
   public void ABoardHasAnAdvertisementWhenItIsCreated() {
-    board = new AdvertisementBoard();
     int obtainedValue = board.numberOfPublishedAdvertisements();
     assertEquals(1,obtainedValue);
   }
 
   @Test
   public void PublishAnAdvertisementByTheCompanyIncreasesTheNumberOfAdvertisementsByOne() {
+
     int expectedValue = board.numberOfPublishedAdvertisements()+1;
     Advertisement advertisement = new Advertisement("Anuncio de The Company","soy un anuncio", "THE Company");
-    board.publish(advertisement,null,null);
+    board.publish(advertisement,advDatabMock,paymDatabMock);
     int obtainedValue = board.numberOfPublishedAdvertisements();
     assertEquals(expectedValue, obtainedValue);
   }
 
   @Test
-  public void WhenAnAdvertiserHasNoFoundsTheAdvertisementIsNotPublished() {}
+  public void WhenAnAdvertiserHasNoFoundsTheAdvertisementIsNotPublished() {
+    Advertisement advertisement = new Advertisement("Chapuzas a domicilio","Destrozamos tu casa", "Pepe Gotera y Otilio");
+
+    when(advDatabMock.findAdviser(advertisement.advertiser)).thenReturn(true);
+    when(paymDatabMock.advertiserHasFunds(advertisement.advertiser)).thenReturn(false);
+
+    int expectedValue = board.numberOfPublishedAdvertisements();
+
+    board.publish(advertisement,advDatabMock,paymDatabMock);
+    int obtainedValue = board.numberOfPublishedAdvertisements();
+    assertEquals(expectedValue, obtainedValue);
+  }
 
   @Test
-  public void AnAdvertisementIsPublishedIfTheAdvertiserIsRegisteredAndHasFunds() {}
+  public void AnAdvertisementIsPublishedIfTheAdvertiserIsRegisteredAndHasFunds() {
+
+    Advertisement advertisement = new Advertisement("Anuncio de Robin Robot","Me llaman Bender", "Robin Robot");
+
+    when(advDatabMock.findAdviser(advertisement.advertiser)).thenReturn(true);
+    when(paymDatabMock.advertiserHasFunds(advertisement.advertiser)).thenReturn(true);
+
+    int expectedValue = board.numberOfPublishedAdvertisements()+1;
+
+    board.publish(advertisement,advDatabMock,paymDatabMock);
+    int obtainedValue = board.numberOfPublishedAdvertisements();
+    assertEquals(expectedValue, obtainedValue);
+
+    verify(paymDatabMock, times(1)).advertisementPublished(anyString());
+  }
 
   @Test
-  public void WhenTheOwnerMakesTwoPublicationsAndTheFirstOneIsDeletedItIsNotInTheBoard() {}
+  public void WhenTheOwnerMakesTwoPublicationsAndTheFirstOneIsDeletedItIsNotInTheBoard() {
+    Advertisement advertisement1 = new Advertisement("Anuncio de The Company 1","soy el primer anuncio", "THE Company");
+    Advertisement advertisement2 = new Advertisement("Anuncio de The Company 2","soy el segundo anuncio", "THE Company");
+
+    board.publish(advertisement1,advDatabMock,paymDatabMock);
+    board.publish(advertisement2,advDatabMock,paymDatabMock);
+    board.deleteAdvertisement("Anuncio de The Company 1", "THE Company");
+
+    assertNull(board.findByTitle("Anuncio de The Company 1"));
+  }
 
   @Test
   public void AnExistingAdvertisementIsNotPublished() {}
