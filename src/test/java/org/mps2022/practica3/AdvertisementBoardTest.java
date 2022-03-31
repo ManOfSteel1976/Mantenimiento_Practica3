43ed54048eca0209c84e4c39312ec1fd5a9930aa
ManOfSteel1976 <57708230+ManOfSteel1976@users.noreply.github.com>
2022-03-31 12:00:14 +0200

    test 1

--- src/test/java/org/mps2022/practica3/AdvertisementBoardTest.java
+++ src/test/java/org/mps2022/practica3/AdvertisementBoardTest.java
@@ -1,15 +1,33 @@
 package org.mps2022.practica3;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class AdvertisementBoardTest {
-  @Test
-  public void ABoardHasAnAdvertisementWhenItIsCreated() {}
+
+  public AdvertisementBoard board;
 
   @Test
-  public void PublishAnAdvertisementByTheCompanyIncreasesTheNumberOfAdvertisementsByOne() {}
+  public void ABoardHasAnAdvertisementWhenItIsCreated() {
+    board = new AdvertisementBoard();
+    int obtainedValue = board.numberOfPublishedAdvertisements();
+    assertEquals(1,obtainedValue);
+  }
+
+  @Test
+  public void PublishAnAdvertisementByTheCompanyIncreasesTheNumberOfAdvertisementsByOne() {
+    int expectedValue = board.numberOfPublishedAdvertisements()+1;
+    Advertisement advertisement = new Advertisement("Anuncio de The Company",)
+  }
 
   @Test
   public void WhenAnAdvertiserHasNoFoundsTheAdvertisementIsNotPublished() {}
